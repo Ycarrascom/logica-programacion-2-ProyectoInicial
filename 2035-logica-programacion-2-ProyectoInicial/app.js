15528ca1313109236640671c77c6110bd2819bf4
Ycarrascom <83997332+Ycarrascom@users.noreply.github.com>
2025-08-07 12:50:12 -0300

    prueba de la funcion

--- 2035-logica-programacion-2-ProyectoInicial/app.js
+++ 2035-logica-programacion-2-ProyectoInicial/app.js
@@ -3,3 +3,9 @@ titulo.innerHTML = 'Juego del numero secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+
+//encapsulacion del evento que quiero que se ejecute
+function intentoDeUuario() {
+    alert('click desde el boton.');
+}
