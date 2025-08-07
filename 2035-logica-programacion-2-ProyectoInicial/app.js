dca0fbcb4c8c95cc2bf28bd272620fbf8df42117
Ycarrascom <83997332+Ycarrascom@users.noreply.github.com>
2025-08-07 13:43:52 -0300

    renombramiento de la funcion

---
+++ 2035-logica-programacion-2-ProyectoInicial/app.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+
+//encapsulacion del evento que quiero que se ejecute
+function intentoDeUsuario() {
+    alert('click desde el boton.');
+}
