32b6bf1e5480383f4384ed02c6be7e2cdc412f7b
Ycarrascom <83997332+Ycarrascom@users.noreply.github.com>
2025-08-07 16:09:32 -0300

    automatizacion de la asignacion de textos

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-let titulo = document.querySelector('h1');
+/*let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Juego del numero secreto';
 
 let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un numero del 1 al 10';
+parrafo.innerHTML = 'Indica un numero del 1 al 10';*/
 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}    
 
 //encapsulacion del evento que quiero que se ejecute
 function intentoDeUuario() {
     alert('click desde el boton.');
 }
+
+asignarTextoElemento('h1', 'Juego del numero secreto');
+asignarTextoElemento('p', 'Indica un numero del 1 al 10');
